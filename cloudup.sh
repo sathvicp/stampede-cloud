839a37c89997251d26fb539d40ef534240f0de28
Sathvic <sathvic.p@gmail.com>
2020-08-02 09:48:22 +0200

    Changed service id to container id for cron

--- cloudup.sh
+++ cloudup.sh
@@ -28,17 +28,29 @@ then
     exit 3
 fi
 
+echo "Checks passed successfully."
+echo "."
+echo "Deploying nextcloud stack..."
+
 # Deploy docker stack
-if docker stack deploy --compose-file=$FILE nextcloud
+if docker stack deploy --compose-file=$FILE nextcloud &> /dev/null
 then
     # On successfull stack deployment
-    NEXTCLOUD_SERVICE_ID=$(docker stack services -f name=nextcloud_app --format "{{.ID}}" nextcloud)
-    docker exec -d $NEXTCLOUD_SERVICE_ID sh -c "/cron.sh"
+    echo "Stack deployed successfully. Starting cron service for nextcloud_app container"
+    if docker exec -d nextcloud_app.1.$(docker service ps -f 'name=nextcloud_app.1' nextcloud_app -q --no-trunc | head -n1) sh -c "/cron.sh" &> /dev/null
+    then
+	echo "."
+	echo "Cron service started successfully."
+    else
+	echoerr "[ERROR] Cron service could not be started successfully"
+	exit 5
+    fi
 else
     echoerr "[ERROR] Could not start the docker stack"
     exit 4
 fi
 
-echo "Stack deployed successfully"
+echo "."
+echo "Cloud deployed successfully."
 exit
 
